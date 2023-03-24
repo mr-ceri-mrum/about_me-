f28ae9232e82748f9b80f48b18e078427620516f
makabake9 <zhunussoffilias0216@gmail.com>
2023-03-24 13:38:15 +0600

    add Swich info

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -7,19 +7,19 @@ const Contacts = () => {
                 <ul className="content-list">
                     <li className="content-list__item">
                         <h2 className="title-2">Location</h2>
-                        <p>Moscow, Russia</p>
+                        <p>Almaty</p>
                     </li>
                     <li className="content-list__item">
                         <h2 className="title-2">Telegram / WhatsApp</h2>
                         <p>
-                            <a href="tel:+79051234567">+7 (905) 123-45-67</a>
+                            <a href="tel:+777777777">+7 (777)7777777</a>
                         </p>
                     </li>
                     <li className="content-list__item">
                         <h2 className="title-2">Email</h2>
                         <p>
-                            <a href="mailto:webdev@protonmail.com">
-                                webdev@protonmail.com
+                            <a href="examplemail@mail.com">
+                                examplemail@mail.com
                             </a>
                         </p>
                     </li>
